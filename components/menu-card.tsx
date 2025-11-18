3b6477a91c5f21ddc28b85e4c846ffdb04b75e7b
Asanshay Gupta <46036121+SuperAce100@users.noreply.github.com>
2025-11-18 11:09:44 -0800

    Interaction

--- components/menu-card.tsx
+++ components/menu-card.tsx
@@ -15,13 +15,13 @@ export default function MenuCard({
   return (
     <Link
       href={href}
-      className="flex-1 aspect-square rounded-2xl border border-transparent hover:border-white/30 hover:bg-white/5  transition-all duration-100 p-6 relative overflow-hidden"
+      className="flex-1 aspect-square rounded-2xl border border-transparent  hover:bg-white/5  transition-all duration-100 p-6 relative overflow-hidden group"
     >
       <h3 className="text-5xl text-white font-medium tracking-tight absolute bottom-6 left-6 text-center">
         {title}
       </h3>
       <ImageDithering
-        className="absolute inset-0 w-full h-full object-cover mix-blend-screen"
+        className="absolute inset-0 w-full h-full object-cover mix-blend-screen group-hover:scale-105 transition-all duration-300"
         width="100%"
         height="100%"
         image={image}
